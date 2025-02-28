1d3d9f5f0e1ec621935a0caff101bba26484a096
john <ga341946@gmail.com>
2025-03-01 00:47:54 +0800

    運費顯示調整

--- base/templates/themes/default/js/shopping_cart.js
+++ base/templates/themes/default/js/shopping_cart.js
@@ -266,6 +266,7 @@ function handleDeliveryChange() {
         var getyunfei = $("#yunfei").val();
         var getpaytotal = $("#tjine").val();
         let numtotal = Number(getpaytotal);
+        let oryun = Number( $("#yunfei").val());
         if(numtotal<2500)
             {
                 
@@ -292,6 +293,18 @@ function handleDeliveryChange() {
                 
             $("#yunfei").val(85);
             }
+            }
+            else
+            {
+                $('.t_delivery_checked_value').text('NT$ 0');
+                $('#ftd').text('NT$ 0');
+                if(oryun!=0)
+                {
+                    var newpay=numtotal-oryun;
+                    $("#tjine").val(newpay);
+                    $('#tjone').text('NT$'+newpay);
+                    $('#tjtwo').text('NT$'+newpay);
+                }
             }
         $('.checked-info-text').text('• 含營業稅').removeClass('font-weight-bold text-danger h4_5');
 
